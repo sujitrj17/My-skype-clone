1fe66d2b9132cc341b39738186b9d79e106747ac
sujitrj17 <sujit007@gmail.com>
2020-05-29 16:29:38 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:skype_clone/resources/firebase_repository.dart';
+import 'package:skype_clone/screens/home_screen.dart';
+import 'package:skype_clone/screens/login_screen.dart';
+import 'package:skype_clone/screens/search_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  FirebaseRepository _repository = FirebaseRepository();
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      initialRoute: '/',
+      title: "Skype",
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/search_screen':(context)=> SearchScreen(),
+      },
+      home: FutureBuilder(
+        future: _repository.getCurrentUser(),
+        builder: (context, AsyncSnapshot<FirebaseUser> snapshot){
+          if(snapshot.hasData){
+            return HomeScreen();
+          }else {
+            return LoginScreen();
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/util/utilities.dart
@@ -0,0 +1,14 @@
+
+class Utils {
+  static String getUsername(String email){
+    String tmp = "sujit.jagdale@sumasoft.net";
+    return "live:${email.split('@')[0]}";
+  }
+
+  static String getInitials(String name) {
+    List<String> nameSplit = name.split(" ");
+    String firstNameInitial = nameSplit[0][0];
+    String lastNameInitial = nameSplit[1][0];
+    return firstNameInitial + lastNameInitial;
+  }
+}
